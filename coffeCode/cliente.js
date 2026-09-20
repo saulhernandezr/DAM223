c921825d9a785b6b2935747823d6e3657eda6d01
saulhernandezr <125054357@upq.edu.mx>
2026-09-19 20:19:00 -0600

    proyecto2 coffecode

--- coffeCode/cliente.js
+++ coffeCode/cliente.js
@@ -1,32 +1,37 @@
-const mensaje = `Hola, bienvenido. Elige tu pedido`;
-const pedidos = [];
-let pacumulado = 0;
-console.log("===============================");
-console.log(mensaje);
+import {menuCocina}from "./cocina.js";
 
+import {pedidos,agregarPedido,modificarPedido,eliminarPedido,listarPedidos
+} from "./caja.js";
+import readline from "readline";
 
-function agregarPedido(nombre, precio) {
-    pedidos.push({
-        nombre: nombre,
-        precio: precio
-    });
-    pacumulado += precio;
+const entrada = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    console.log(`Pedido agregado: ${nombre} - Precio: $${precio}`);
-    console.log(`Total acumulado: $${pacumulado}`);
+function menu(){
+console.log("\n!! MENU !!");
+    console.log("1.Gestionar Cocina");
+    console.log("2. Listar pedido");
+    console.log("3. Salir");
+entrada.question("Elige una opcion? ",function(opcion){
+
+if(opcion=="1"){
+menuCocina(entrada,menu,agregarPedido,modificarPedido,eliminarPedido,listarPedidos,pedidos);
+
+        }else if(opcion=="2"){
+            listarPedidos();
+            menu();
+
+        }else if(opcion == "3"){
+    entrada.close();
+
+        }else {
+    console.log("no valido");
+menu();
+
+        }
+     });
 }
 
-function listarPedidos() {
-    return pedidos;
-}
-
-function totalAcumulado() {
-    return pacumulado;
-}
-
-module.exports = {
-    pedidos,
-    agregarPedido,
-    listarPedidos,
-    totalAcumulado
-};
+menu();
