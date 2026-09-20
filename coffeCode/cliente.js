b49fa913f28f9a53bf776298a567df468bbf10ac
saulhernandezr <125054357@upq.edu.mx>
2026-09-19 18:08:40 -0600

    practica2

--- coffeCode/cliente.js
+++ coffeCode/cliente.js
@@ -1,47 +1,32 @@
+const mensaje = `Hola, bienvenido. Elige tu pedido`;
 const pedidos = [];
 let pacumulado = 0;
-const mensaje = `Hola, bienvenido. Elige tu pedido`;
-
 console.log("===============================");
 console.log(mensaje);
+
+
 function agregarPedido(nombre, precio) {
     pedidos.push({
         nombre: nombre,
         precio: precio
     });
     pacumulado += precio;
+
     console.log(`Pedido agregado: ${nombre} - Precio: $${precio}`);
     console.log(`Total acumulado: $${pacumulado}`);
 }
+
 function listarPedidos() {
-
-    console.log("\n==========pedidos==========");
-
-    pedidos.forEach((pedido, indice) => {
-        console.log(
-            `${indice + 1}. ${pedido.nombre} - $${pedido.precio}`
-        );
-    });
-
     return pedidos;
 }
 
 function totalAcumulado() {
     return pacumulado;
 }
-function consultarProducto(nombre) {
-
-    const producto = pedidos.find(
-        pedido => pedido.nombre.toLowerCase() === nombre.toLowerCase()
-    );
-
-    return producto;
-}
 
 module.exports = {
     pedidos,
     agregarPedido,
     listarPedidos,
-    totalAcumulado,
-    consultarProducto
+    totalAcumulado
 };
